fdcc6359a8e7b542fef8ce850700d386c03cd112
bisset <bisset@metier.jp>
2022-04-05 21:57:12 +0900

    add tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+}
